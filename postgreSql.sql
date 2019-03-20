52242237ffb67b5a8ac57592af5e78cccc95537a
baihua <baihua_2011@163.com>
2019-03-20 17:09:44 +0800

    Updated database configuration

---
+++ postgreSql.sql
@@ -0,0 +1,127 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : homestead-PostgreSQL
+ Source Server Type    : PostgreSQL
+ Source Server Version : 100006
+ Source Host           : localhost:2205
+ Source Catalog        : laravel_weibo
+ Source Schema         : public
+
+ Target Server Type    : PostgreSQL
+ Target Server Version : 100006
+ File Encoding         : 65001
+
+ Date: 20/03/2019 17:03:25
+*/
+
+
+-- ----------------------------
+-- Sequence structure for migrations_id_seq
+-- ----------------------------
+DROP SEQUENCE IF EXISTS "public"."migrations_id_seq";
+CREATE SEQUENCE "public"."migrations_id_seq" 
+INCREMENT 1
+MINVALUE  1
+MAXVALUE 2147483647
+START 1
+CACHE 1;
+
+-- ----------------------------
+-- Sequence structure for users_id_seq
+-- ----------------------------
+DROP SEQUENCE IF EXISTS "public"."users_id_seq";
+CREATE SEQUENCE "public"."users_id_seq" 
+INCREMENT 1
+MINVALUE  1
+MAXVALUE 2147483647
+START 1
+CACHE 1;
+
+-- ----------------------------
+-- Table structure for migrations
+-- ----------------------------
+DROP TABLE IF EXISTS "public"."migrations";
+CREATE TABLE "public"."migrations" (
+  "id" int4 NOT NULL DEFAULT nextval('migrations_id_seq'::regclass),
+  "migration" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
+  "batch" int4 NOT NULL
+)
+;
+ALTER TABLE "public"."migrations" OWNER TO "homestead";
+
+-- ----------------------------
+-- Records of migrations
+-- ----------------------------
+BEGIN;
+INSERT INTO "public"."migrations" VALUES (1, '2014_10_12_000000_create_users_table', 1);
+INSERT INTO "public"."migrations" VALUES (2, '2014_10_12_100000_create_password_resets_table', 1);
+COMMIT;
+
+-- ----------------------------
+-- Table structure for password_resets
+-- ----------------------------
+DROP TABLE IF EXISTS "public"."password_resets";
+CREATE TABLE "public"."password_resets" (
+  "email" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
+  "token" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
+  "created_at" timestamp(0)
+)
+;
+ALTER TABLE "public"."password_resets" OWNER TO "homestead";
+
+-- ----------------------------
+-- Table structure for users
+-- ----------------------------
+DROP TABLE IF EXISTS "public"."users";
+CREATE TABLE "public"."users" (
+  "id" int4 NOT NULL DEFAULT nextval('users_id_seq'::regclass),
+  "name" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
+  "email" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
+  "email_verified_at" timestamp(0),
+  "password" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
+  "remember_token" varchar(100) COLLATE "pg_catalog"."default",
+  "created_at" timestamp(0),
+  "updated_at" timestamp(0)
+)
+;
+ALTER TABLE "public"."users" OWNER TO "homestead";
+
+-- ----------------------------
+-- Records of users
+-- ----------------------------
+BEGIN;
+INSERT INTO "public"."users" VALUES (1, 'user01', 'user01@test.com', NULL, '$2y$10$BJsn.2/W/R2MD.OlbxnUxOq8a8H2K3xWNtNgkYJwfebRkIdzg7/eW', NULL, '2019-03-20 08:58:57', '2019-03-20 08:58:57');
+COMMIT;
+
+-- ----------------------------
+-- Alter sequences owned by
+-- ----------------------------
+ALTER SEQUENCE "public"."migrations_id_seq"
+OWNED BY "public"."migrations"."id";
+SELECT setval('"public"."migrations_id_seq"', 3, true);
+ALTER SEQUENCE "public"."users_id_seq"
+OWNED BY "public"."users"."id";
+SELECT setval('"public"."users_id_seq"', 2, true);
+
+-- ----------------------------
+-- Primary Key structure for table migrations
+-- ----------------------------
+ALTER TABLE "public"."migrations" ADD CONSTRAINT "migrations_pkey" PRIMARY KEY ("id");
+
+-- ----------------------------
+-- Indexes structure for table password_resets
+-- ----------------------------
+CREATE INDEX "password_resets_email_index" ON "public"."password_resets" USING btree (
+  "email" COLLATE "pg_catalog"."default" "pg_catalog"."text_ops" ASC NULLS LAST
+);
+
+-- ----------------------------
+-- Uniques structure for table users
+-- ----------------------------
+ALTER TABLE "public"."users" ADD CONSTRAINT "users_email_unique" UNIQUE ("email");
+
+-- ----------------------------
+-- Primary Key structure for table users
+-- ----------------------------
+ALTER TABLE "public"."users" ADD CONSTRAINT "users_pkey" PRIMARY KEY ("id");
