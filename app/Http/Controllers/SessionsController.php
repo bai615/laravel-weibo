ac18276a3c122771eff7f233a17f1eebed2bba77
baihua <baihua_2011@163.com>
2019-03-20 18:01:45 +0800

    用户退出登录

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -13,6 +13,7 @@ class SessionsController extends Controller
         return view('sessions.create');
     }
 
+    // 登录处理
     public function store(Request $request)
     {
         $credentials = $this->validate($request,[
@@ -30,6 +31,13 @@ class SessionsController extends Controller
             return redirect()->route('users.show', [Auth::user()]);
         }
 
-        return;
+    }
+
+    // 退出登录
+    public function destroy()
+    {
+        Auth::logout();
+        session()->flash('success', '您已成功退出！');
+        return redirect('login');
     }
 }
